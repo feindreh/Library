f9664c95685c15cab4ca74a2cb5fa372395ef263
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-17 06:37:57 +0100

    display books function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -16,10 +17,20 @@ function addBook(title, author, pages, read) {
 
 function displayLibrary() {
   // loop through library and render the books
-  const div = document.createElement('div');
-  div.innerText = 'it works';
-  document.getElementById('book-shelf').appendChild(div);
+  document.getElementById('book-shelf').innerHTML = '';
+  for (const book of myLibrary) {
+    const div = document.createElement('div');
+    for (const part in book) {
+      console.log(part);
+      div.innerText += `${book[part]}`;
+    }
+    document.getElementById('book-shelf').appendChild(div);
+  }
 }
 
 const Launch = document.querySelector('#Launch');
 Launch.addEventListener('click', () => { displayLibrary(); });
+
+addBook('harry', 'JRK', 185, 'true');
+addBook('Hobit', 'Tolkien', 385, 'true');
+addBook('1984', 'George', 125, 'false');
